133c7b09c057c675a778d43491c25e5f5b9f967c
Benjamin Lowry <Lowry_46@hotmail.com>
2023-05-10 16:04:50 -0400

    first commit

---
+++ movie_library/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import InputRequired, NumberRange
+
+
+class MovieForm(FlaskForm):
+    title = StringField("Title", validators=[InputRequired()])
+    director = StringField("Director", validators=[InputRequired()])
+    year = IntegerField("Year", validators=[
+            InputRequired(),
+            NumberRange(min=1878, message="Please enter in format YYYY")])
+    submit = SubmitField("Add Movie")
